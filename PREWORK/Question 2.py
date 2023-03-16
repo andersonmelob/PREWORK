feb11fc74782051901e9628c5e0e469130df80d8
andersonmelob <127802049+andersonmelob@users.noreply.github.com>
2023-03-16 11:15:26 -0400

    Add files via upload

---
+++ PREWORK/Question 2.py
@@ -0,0 +1,7 @@
+# 2) Write a python function, first_odds that prints the odd numbers from 1-100 and returns nothing.
+
+def odd_numbers():
+    for i in range(1, 101, 2):
+        print(i)
+        
+odd_numbers()
